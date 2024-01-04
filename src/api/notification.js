d85dcf9656ce9179539e450a1c845646ab9ad9de
FRANCIS_PC\francins <francis@guardisai.com>
2024-01-03 18:24:16 -0600

    Add Redux

--- src/api/notification.js
+++ src/api/notification.js
@@ -1,6 +1,30 @@
 import axios from "axios";
 
 export default {
+  async getNotifications() {
+    const request = axios
+      .get(`${localStorage.getItem("cfUrl")}notifications`, null)
+      .then(function (response) {
+        if (response == null || response.data.length === 0) {
+          console.log("No events found!");
+          return null;
+        } else {
+          let notification_list = response.data.filter(
+            (n) => n.user_feedback === null
+          );
+
+          return notification_list;
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    const notifications = await request;
+
+    return notifications;
+  },
+
   async getNotificationTypes() {
     const request = axios
       .get(`${localStorage.getItem("cfUrl")}notifications/types`)
@@ -9,8 +33,7 @@ export default {
           console.log("No notifications types found!");
           return null;
         } else {
-
-            const notificationTypes = response.data.sort((a, b) =>
+          const notificationTypes = response.data.sort((a, b) =>
             a.human_readable.localeCompare(b.human_readable)
           );
 
